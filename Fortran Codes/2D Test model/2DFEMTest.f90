a61b3e5b311461364268b1de73c5558b1deffaec
Anil Aksu <aaa293@cornell.edu>
2016-10-26 17:17:51 +0300

    Start of 2D FEM Model
    This is gonna be model for 2D FEM for deformable structure element
    under the effect of the plasma flow

---
+++ Fortran Codes/2D Test model/2DFEMTest.f90
@@ -0,0 +1,8 @@
+Program FEM2DTest
+	!this program is written to test 2-D deformable elements
+	!it is an initial model to test effects of plasma on thruster structures
+	integer i,j,k
+	 
+	
+	
+End Program FEM2DTest	
