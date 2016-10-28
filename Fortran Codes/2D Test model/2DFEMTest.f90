efcf5b0e89a5a1530fc42fcbd5c50ba8e6eb5c20
Anil Aksu <aaa293@cornell.edu>
2016-10-28 10:24:37 +0300

    First Kickout in 2D SEM Fluid Structure Interaction
    It is used to run gfortran with makefile

--- Fortran Codes/2D Test model/2DFEMTest.f90
+++ Fortran Codes/2D Test model/2DFEMTest.f90
@@ -2,7 +2,28 @@ Program FEM2DTest
 	!this program is written to test 2-D deformable elements
 	!it is an initial model to test effects of plasma on thruster structures
 	integer i,j,k
-	 
+	! the start and the end point of the grid 
+	real*8 x_ini, x_end
+	! the grid points array
+	real*8, allocatable:: x_grid(:)
+	! the number of points in grid 
+	integer N
 	
+	!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	!                                                     !
+	!     2D Spectral Element Model Simulation Parameter  !
+	!                                                     !
+	!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	
+	N=9
+	x_ini=0.1
+	x_end=0.9
+	
+	allocate(x_grid(N))
+	! let's generate a sample grid  
+	call	OneDGrid(x_ini,x_end,x_grid,N)
+	
+	print*, "Sample Grid Points"
+	print*, x_grid
 	
 End Program FEM2DTest	
---
+++ Fortran Codes/2D Test model/MeshTransform.f90
@@ -0,0 +1,24 @@
+subroutine OneDGrid(x_ini,x_end,x_grid,N)
+	integer i
+	! the number of grid points
+	integer, intent(in):: N
+	!the start and end coordinates of mesh
+	real*8, intent(in):: x_ini,x_end
+	!the grid coordinate array
+	real*8, intent(inout),dimension(N):: x_grid
+	! the length of domain and the uniform distance between points
+	real*8 domLength, dx
+	
+	! the domain length is calculated as
+	domLength=x_end -x_ini
+	! let's calculate the uniform distance between points
+	dx=domLength/(N-1)
+	! first point of grid starts with x_ini
+	x_grid(1)=x_ini
+	
+	do i=1,(N-1)
+		x_grid(i+1)=x_grid(i)+dx
+		!print*, x_grid(i+1)
+	end do
+		
+end subroutine OneDGrid
